07c8ceba694c514f6d05fd5b88e031afe71abcc9
Shawn Kebel <fxrender@outlook.com>
2022-03-14 17:44:34 -0500

    :sparkles: Add `Dashboard` page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Entry } from './pages/entry/Entry.page';
 import { DefaultLayout } from './layout/DefaultLayout';
+import { Dashboard } from './pages/dashboard/Dashboard.page';
 import './App.css';
+
 
 
 function App() {
@@ -9,7 +11,7 @@ function App() {
     <div className="App">
       {/* <Entry /> */}
       <DefaultLayout>
-        // Dashboard
+        <Dashboard />
       </DefaultLayout>
     </div>
   );
