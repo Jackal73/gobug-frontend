87f9899ea9ceb5b5976b7c57834c22c4df131522
Shawn Kebel <fxrender@outlook.com>
2022-03-14 18:58:46 -0500

    :necktie: Add `Breadcrumb`

--- src/pages/dashboard/Dashboard.page.js
+++ src/pages/dashboard/Dashboard.page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Col, Container, Row, Button } from 'react-bootstrap';
 import { TicketTable } from '../../components/ticket-table/TicketTable.comp';
 import tickets from '../../assets/data/dummy-tickets.json';
+import { PageBreadcrumb } from '../../components/breadcrumb/Breadcrumb.comp';
+
 
 
 
@@ -9,8 +11,13 @@ export const Dashboard = () => {
   return (
     <Container>
       <Row>
+        <Col>
+          <PageBreadcrumb page="Dashboard" />
+        </Col>
+      </Row>
+      <Row>
         <Col className="text-center mt-5 mb-2">
-          <Button variant="info" style={{fontSize: '2rem', padding : '10px 30px'}}>Add New Ticket</Button>
+          <Button variant="info" style={{ fontSize: "2rem", padding : '10px 30px' }}>Add New Ticket</Button>
         </Col>
       </Row>
       <Row>
