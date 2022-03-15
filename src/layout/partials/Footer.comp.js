65f3b0faa396fd663776127c163f1005694f64b4
Shawn Kebel <fxrender@outlook.com>
2022-03-15 18:07:05 -0500

    :lipstick: Change `logo` and style adjustments.

--- src/layout/partials/Footer.comp.js
+++ src/layout/partials/Footer.comp.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import tikLogo from '../../assets/img/tikLogo.png';
 
 export const Footer = () => {
-  return <div className="text-center copyright">&copy;Tikkit. All Rights Reserved. 2022.</div>
+  return <div className="text-center copyright">
+    {/* <a href="https://www.kbldesigners.com" alt="link">kblDesigners</a> */}
+    <img
+      src={tikLogo}
+      className="mb-1 ml-1 mr-1" alt="logo"
+      height="26px"
+    />
+     &copy;2022. All Rights Reserved.
+  </div>
 
 }
